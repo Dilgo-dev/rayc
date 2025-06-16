d7951d65ecc60ae864e3b76d66e4902e55f92af9
Dilgo-dev <dilgopierre@gmail.com>
2025-06-16 10:40:43 +0200

    feat: Implement ray casting function for ray tracing logic
    - Added the `cast_ray` function to handle ray movement and wall detection within the map.
    - Included calculations for ray direction and distance to the wall upon collision.
    - Established a continuous loop for ray advancement and boundary checks to enhance raycasting functionality.

---
+++ src/raycasting/cast_ray.c
@@ -0,0 +1,32 @@
+#include <math.h>
+#include "raycasting.h"
+
+float cast_ray(int map[4][4], float pos_x, float pos_y, float ray_angle) {
+    // Calculate direction of ray
+    float ray_dx = cos(ray_angle);
+    float ray_dy = sin(ray_angle);
+
+    // Rayon start position
+    float ray_x = pos_x;
+    float ray_y = pos_y;
+
+    while (1) {
+
+        // Move the rayon forward
+        ray_x += ray_dx * STEP_SIZE;
+        ray_y += ray_dy * STEP_SIZE;
+
+        // Convert into map position
+        int grid_x = (int)ray_x;
+        int grid_y = (int)ray_y;
+
+        // Check if it's out of bounderies
+        if (grid_x < 0 || grid_x >= 4 || grid_y < 0 || grid_y >= 4) return 0.0;
+
+        // Check if ray touch a wall
+        if (map[grid_y][grid_x] == 1) {
+            return sqrt(pow(ray_x - pos_x, 2) + pow(ray_y - pos_y, 2));
+        }
+
+    }
+}
